11c888bab3028849fb5cca426ccfacef288e76f7
EJ <estanislaojose.suarez@eclectustech.net>
2024-09-12 23:12:16 +0800

    initialize app

---
+++ src/pages/login/login-page.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./login-page.css";
+
+const LoginPage = () => {
+  return (
+    <div className="login-page">
+      <h1>Login Page</h1>
+      <p>Welcome to the Login Page!</p>
+    </div>
+  );
+};
+
+export default LoginPage;
