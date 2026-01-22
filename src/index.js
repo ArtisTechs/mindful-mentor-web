b63b657ed227a1060f3bbcf46e7064a9885f8b5a
EJ <estanislaojose.suarez@eclectustech.net>
2026-01-22 09:44:55 +0800

    update fontawesome dependency to version 7.1.0 and fix import style in index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "./shared";
-import "@fortawesome/fontawesome-free/css/all.min.css";
+import '@fortawesome/fontawesome-free/css/all.min.css';
 import { GlobalStateProvider } from "./shared/context";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
